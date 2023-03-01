3412a0344e99b02c66e9819a08e25135f1f1a7c6
Thomas Burke <piecraft24@gmail.com>
2023-03-01 16:46:02 +0000

    Removed model buttons for now.
    All models are the same for some reason.

--- src/Components/Modals/Projects/Breakit_Modal.js
+++ src/Components/Modals/Projects/Breakit_Modal.js
@@ -27,11 +27,9 @@ function Breakit_Modal() {
                 
                 <div class="p-6 space-y-6">
                     <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                        With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                        This Vr game was built for Atu. The goal of this game is to destroy as much as possible  with out being caught by the guard. There are vases and paintings to destroy.
                     </p>
-                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                        The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                    </p>
+                    
                 </div>
                 
                 <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
