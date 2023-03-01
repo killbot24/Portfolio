475c93794e48e3c5d2f33e3fb6961522bc92e95a
Thomas Burke <piecraft24@gmail.com>
2023-03-01 16:46:10 +0000

    Added WIP

--- src/Components/Top.js
+++ src/Components/Top.js
@@ -4,6 +4,7 @@ function Top() {
  return (
  
    <div class="Top-Section mt-5 relative mb-5 ">
+    <h1 className=' bg-red-500 text-white text-center'>WIP</h1>
     <img class="w-3/4 justify-center mx-auto relative " src='https://img.itch.zone/aW1hZ2UvMTQ4NTAwNS84ODU0MjE1LnBuZw==/original/VlmpCG.png'></img>
     <hr class="w-1/2 justify-center mx-auto border-r-8 absolute  text-white bottom-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 invisible lg:visible"></hr>
     <h1 class="absolute text-5xl text-white bottom-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 invisible lg:visible">
