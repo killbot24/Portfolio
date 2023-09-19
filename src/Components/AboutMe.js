49e7bb7400eda41f0a1c4681983419b260ed7ace
Thomas Burke <piecraft24@gmail.com>
2023-09-19 20:42:33 +0100

    Added interactable past exp

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,18 +1,56 @@
 
 import React, { useState, useEffect } from "react";
-import ExperianceCard from './Modals/Experiance/ExperianceCards';
+
+
 function AboutMe() {
+    const [data, setdata] = useState(null)
+    const jsonData = [
+        {
+            "id": 1,
+            "jobTitle": "Full Stack Developer",
+            "company": "Dapper fox group",
+            "Years": "2023-present",
+            "desc": "My Duties in this role are to design and develop a partner portal to keep track of company media partners, My current stack in this role is React, Node.js, Next.js, Mysql, HTML, CSS, Tailwind.   "
+        },
+        {
+            "id": 2,
+            "jobTitle": "IT Support Specialist",
+            "company": "Dapper fox group",
+            "Years": "2019-present",
+            "desc": "My duties in this role are to answer support tickets that cover a wide range of issues from Connection issues to payment issues & inquiries "
+        },
+        {
+            "id": 3,
+            "jobTitle": "Software Engineer",
+            "company": "Dapper fox group",
+            "Years": "2020-2022",
+            "desc": "During my time in this role my main acchivement was developing a chatfilter that reduced chat infractions by 90% thus freeing up time of the rest of the staff team."
+        }
+    ];
+
+    const getJobInfo = (id) => {
+        const job = jsonData.find(job => job.id === id);
+        return job;
+    };
+
+    const togglemoreinfo = (info) => {
+        setdata(getJobInfo(info))
+    }
 
 
     return (
         <div id='AboutMe' className="bg-gray-900 text-white py-10 lg:py-20">
             <div class="w-full max-w-5xl mx-auto px-6 lg:px-8">
-                <div class="grid lg:grid-cols-2 gap-8 lg:gap-12">
-                    <div class="flex flex-col justify-center">
-                        <h1 class="text-4xl lg:text-5xl font-bold mb-4">About Me</h1>
-                        <p class="text-lg leading-7 mb-6 lg:mb-8">Hello, I'm Thomas Burke, a recent graduate passionate about entrepreneurship and innovation. I'm proud to have achived stage 2 of the Enterprise Ireland Student Awards our group was the only one in the school of science part of our University  , showing my dedication to these interests.
+
+                <div class=" justify-center">
+                    <h1 className="text-4xl lg:text-5xl font-bold mb-4">Hello, I am Thomas Burke</h1>
+                    <h2>Fullstack | Software Engineer | Web developer</h2>
+                    <div className=" bg-gray-800 shadow-md p-2 mt-20 rounded-md">
+                        <h2 class="text-4xl lg:text-2xl  font-bold mb-4">About Me</h2>
+                        <p class="text-lg leading-7  mb-6 lg:mb-8">Hello, I'm Thomas Burke, a recent graduate passionate about entrepreneurship and innovation. I'm proud to have achived stage 2 of the Enterprise Ireland Student Awards our group was the only one in the school of science part of our University  , showing my dedication to these interests.
                             As a freelance developer for the past two years, I've specialized in Java development and website design</p>
-                        <div class="flex  mb-1">
+
+                        <div class="flex mb-1 bg-slate-900 p-2 rounded-md">
                             <a href="https://github.com/killbot24" target="_blank" rel="noopener noreferrer" class="mr-4">
                                 <img src="Github.svg" alt="GitHub" class="w-10" />
                             </a>
@@ -21,19 +59,15 @@ function AboutMe() {
                             </a>
                         </div>
                     </div>
-                    <div class="flex justify-center items-center">
-                        <img src="/FacePic.jpg" alt="Thomas Burke" class="rounded-full h-64 w-64 object-cover object-middle mx-auto lg:h-80 lg:w-80 shadow-xl" />
-
-
-
-                    </div>
                 </div>
+
+
                 <div class="mt-16 mb-8">
                     <h2 class="text-4xl lg:text-5xl font-bold text-center">Experience</h2>
 
                     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4 gap-6">
 
-                        <div class="flex bg-gray-800 bg-opacity-70 rounded-lg shadow-md p-6">
+                        <div onClick={() => togglemoreinfo(1)} class="flex bg-gray-800 bg-opacity-70 rounded-lg shadow-md p-6 hover:bg-gray-700">
                             <div class="w-full">
                                 <h3 class="text-xl font-bold mb-2">Fullstack Software Engineer</h3>
                                 <h4 class="text-gray-500 text-sm mb-4">Dapper Fox Group</h4>
@@ -47,7 +81,7 @@ function AboutMe() {
                         </div>
 
 
-                        <div class="flex bg-gray-800 bg-opacity-70 rounded-lg shadow-md p-6">
+                        <div onClick={() => togglemoreinfo(2)} class="flex bg-gray-800 bg-opacity-70 rounded-lg shadow-md p-6 hover:bg-gray-700">
                             <div class="w-full">
                                 <h3 class="text-xl font-bold mb-2">IT Support Specialist</h3>
                                 <h4 class="text-gray-500 text-sm mb-4">Dapper Fox Group</h4>
@@ -59,7 +93,7 @@ function AboutMe() {
                                 </div>
                             </div>
                         </div>
-                        <div class="flex bg-gray-800 bg-opacity-70 rounded-lg shadow-md p-6">
+                        <div onClick={() => togglemoreinfo(3)} class="flex bg-gray-800 bg-opacity-70 rounded-lg shadow-md p-6 hover:bg-gray-700">
                             <div class="w-full">
                                 <h3 class="text-xl font-bold mb-2">Software Engineer</h3>
                                 <h4 class="text-gray-500 text-sm mb-4">Dapper Fox Group</h4>
@@ -71,6 +105,23 @@ function AboutMe() {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className=" bg-slate-700 p-2 mt-3 rounded-md">
+                        {data ? (
+                            <>
+                                <div class=" flex justify-end h-fit">
+                                    <div class="bg-gray-200 rounded-md p-2">
+                                        <p class="text-gray-800 font-bold text-md">{data.Years}</p>
+                                    </div>
+                                </div>
+                                <h1 class="">{data.jobTitle}</h1>
+                                <p>{data.company}</p>
+                                <p class="bg-slate-800 p-2 rounded-lg">{data.desc}</p>
+
+                            </>
+                        ) : (
+                            <p>Click on a postion to see more</p>
+                        )}
                     </div>
                 </div>
                 <h2 class="text-4xl lg:text-5xl font-bold text-center">Education</h2>
